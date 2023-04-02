320fceeabb89db5976ee259b926d110d5ec106b7
Vojtěch Lukáš <vlukas@hotmail.cz>
2023-04-02 12:41:29 +0200

    not sure

--- Controller/Populator.php
+++ Controller/Populator.php
@@ -3,14 +3,15 @@ class Populator
 {
     public function populateTask($task)
     {
-        // foreach ($task as $row)
-        // {
-        //     echo $row . " \n";
-        // }
-        switch ($task['code'])
+        if ($task['code']) $code = $task['code'];
+        switch ($code)
         {
+            case 'dh':
+                // $values = logic->getDHvalues()
+                $task['description'] = preg_replace('/(\$)\d+/i', 'DH_VALUE',$task['description']);
             default:
-            $task['description'] = preg_replace('/(\$)\d+/i', 'VALUE',$task['description']);
+                $task['description'] = preg_replace('/(\$)\d+/i', 'DEFAULT',$task['description']);
+            break;
         }
         return $task;
     }
